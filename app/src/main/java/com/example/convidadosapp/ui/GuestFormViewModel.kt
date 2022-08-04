62276e1c5cdd2ccb5df9a8a0e07212b25522b59a
Anderson <anderson.vilhena0@gmail.com>
2022-08-04 11:09:50 -0300

    Adição do botão save

---
+++ app/src/main/java/com/example/convidadosapp/ui/GuestFormViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.convidadosapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel : ViewModel() {
+
+
+}
--- app/src/main/java/com/example/convidadosapp/ui/GuestformActivity.kt
+++ app/src/main/java/com/example/convidadosapp/ui/GuestformActivity.kt
@@ -2,11 +2,30 @@ package com.example.convidadosapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.example.convidadosapp.R
+import com.example.convidadosapp.databinding.ActivityGuestformBinding
 
-class GuestformActivity : AppCompatActivity() {
+class GuestformActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestformBinding
+    private lateinit var viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guestform)
+
+        binding = ActivityGuestformBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.saveButton.setOnClickListener(this)
+        binding.buttonPresent.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.save_button){
+
+        }
     }
 }
