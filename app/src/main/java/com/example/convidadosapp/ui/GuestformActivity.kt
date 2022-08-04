bffcdc585c1803906247c56f6e5c781f704cdd41
Anderson <anderson.vilhena0@gmail.com>
2022-08-04 09:41:21 -0300

    alterações nas fragments

---
+++ app/src/main/java/com/example/convidadosapp/ui/GuestformActivity.kt
@@ -0,0 +1,11 @@
+package com.example.convidadosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuestformActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guestform)
+    }
+}
